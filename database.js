e076e2ded0534f872472b231449290b6c625e233
cianwallcode <cianwallsemail@gmail.com>
2026-04-06 16:00:01 +0100

    Update database.js

--- database.js
+++ database.js
@@ -5,21 +5,21 @@ const siteData = [
     "name": "Mellifont Abbey",
     "bucket": "Cistercian",
     "plan_url": "mellifont.jpg",
-    "labels": {
-        "church": { "x": 30, "y": 10, "w": 45, "h": 20, "name": "CHURCH", "ratio": "Flat Apse / Square Nave" },
-        "cloister": { "x": 38, "y": 35, "w": 25, "h": 25, "name": "GARTH / CLOISTER", "ratio": "1:1 Square" },
-        "refectory": { "x": 45, "y": 62, "w": 10, "h": 25, "name": "REFECTORY", "ratio": "1:√2 (Vertical)" }
-    }
+    "labels": [
+        { "x": 30, "y": 15, "w": 40, "h": 15, "name": "CHURCH", "ratio": "Square Apse" },
+        { "x": 40, "y": 35, "w": 20, "h": 20, "name": "GARTH", "ratio": "1:1 Square" },
+        { "x": 46, "y": 60, "w": 8, "h": 25, "name": "REFECTORY", "ratio": "1:√2 (90°)" }
+    ]
   },
   {
     "id": 2,
     "name": "Cluny Abbey III",
     "bucket": "Benedictine",
     "plan_url": "cluny.jpg",
-    "labels": {
-        "church": { "x": 20, "y": 5, "w": 65, "h": 25, "name": "CHURCH", "ratio": "Five Aisle / Radiating Chapels" },
-        "cloister": { "x": 35, "y": 35, "w": 30, "h": 25, "name": "GARTH / CLOISTER", "ratio": "1:1.2 Rectangular" },
-        "refectory": { "x": 30, "y": 65, "w": 40, "h": 10, "name": "REFECTORY", "ratio": "1:3 (Parallel)" }
-    }
+    "labels": [
+        { "x": 15, "y": 10, "w": 70, "h": 20, "name": "CHURCH", "ratio": "Radiating Chapels" },
+        { "x": 35, "y": 35, "w": 30, "h": 20, "name": "GARTH", "ratio": "1:1.2" },
+        { "x": 30, "y": 60, "w": 40, "h": 8, "name": "REFECTORY", "ratio": "1:3 (Parallel)" }
+    ]
   }
 ];
