54bc8d57d865b26e3567d9d9183f45187feabede
cianwallcode <cianwallsemail@gmail.com>
2026-04-06 15:02:32 +0100

    Update database.js

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const siteData = [
     "style": "Gothic",
     "coords": [53.7424, -6.4651],
     "plan_type": "Austere / Square Apse",
-    "plan_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Mellifont_Abbey_Plan.jpg/800px-Mellifont_Abbey_Plan.jpg",
+    "plan_url": "mellifont.jpg",
     "notes": "Cistercian simplicity. Flat east end."
   },
   {
@@ -16,7 +16,7 @@ const siteData = [
     "style": "Romanesque",
     "coords": [46.4341, 4.6592],
     "plan_type": "Complex / Chevet",
-    "plan_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Plan_Cluny_III.png/800px-Plan_Cluny_III.png",
+    "plan_url": "cluny.jpg",
     "notes": "Benedictine complexity. Radiating chapels."
   }
 ];
