355ba2274bf4f85146e35f2dd3f28ebadfb95c66
cianwallcode <cianwallsemail@gmail.com>
2026-04-06 15:06:10 +0100

    Update database.js

--- database.js
+++ database.js
@@ -4,7 +4,6 @@ const siteData = [
     "name": "Mellifont Abbey",
     "bucket": "Cistercian",
     "style": "Gothic",
-    "coords": [53.7424, -6.4651],
     "plan_type": "Austere / Square Apse",
     "plan_url": "mellifont.jpg",
     "notes": "Cistercian simplicity. Flat east end."
@@ -14,7 +13,6 @@ const siteData = [
     "name": "Cluny Abbey III",
     "bucket": "Benedictine",
     "style": "Romanesque",
-    "coords": [46.4341, 4.6592],
     "plan_type": "Complex / Chevet",
     "plan_url": "cluny.jpg",
     "notes": "Benedictine complexity. Radiating chapels."
